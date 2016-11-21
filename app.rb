03ce11889315e4f6e1580ac006407e1742ee6723
mpowel <mlp206@gmail.com>
2016-11-21 13:56:43 -0500

    stop tweet

--- app.rb
+++ app.rb
@@ -16,12 +16,12 @@ require 'json'
 
 # require 'Twitter'
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "1YD0nfMoRND60Fin2TdiFvRXi"
-  config.consumer_secret     = "jRFdVzO16XKg0JyXK22C55EVOszeF8waIn8vbrzyAUgcs8eNY7"
-  config.access_token        = "800480043981881345-UteUYcaYoEyWwmKxvnmNGAt3X8u8UHV"
-  config.access_token_secret = "JjE1bjDNujhJLnXAhSyk9TlBdLYbwi3lOY3AcSMVyPUAY"
-end
+# client = Twitter::REST::Client.new do |config|
+#   config.consumer_key        = "1YD0nfMoRND60Fin2TdiFvRXi"
+#   config.consumer_secret     = "jRFdVzO16XKg0JyXK22C55EVOszeF8waIn8vbrzyAUgcs8eNY7"
+#   config.access_token        = "800480043981881345-UteUYcaYoEyWwmKxvnmNGAt3X8u8UHV"
+#   config.access_token_secret = "JjE1bjDNujhJLnXAhSyk9TlBdLYbwi3lOY3AcSMVyPUAY"
+# end
 
 # client.update("I'm posting a tweet!")
 
@@ -32,13 +32,36 @@ end
 # end
 
 search_options = {
-  result_type: "recent",
-  geocode: "39.9525839,-75.1652215,10mi"
+  result_type: "recent"# ,
+#   geocode: "39.9525839,-75.1652215,10mi"
 }
 
-client.search("blacklivesmatter", search_options).take(1).each do |tweet|
-  client.update( "#{tweet.user.screen_name}: #{tweet.text}")
-end
+# client.search("blacklivesmatter", search_options).take(1).each do |tweet|
+#   client.update( "#{tweet.user.screen_name}: #{tweet.text}")
+# end
+
+#remove and block inappropriate users
+exclude "http://"
+blocklist "mean_user, private_user"
+
+# # favorite people who visit your timeline
+# puts "checking my timeline"
+# home_timeline do |tweet|
+#     # i like to favorite things
+#     favorite tweet
+# end
+
+# puts "checking for replies to my tweets and mentions of me"
+
+# # reply to people who tweet at me
+# replies do |tweet|
+#   text = tweet.text
+#   puts "message received: #{text}"
+#   src = text.gsub(/@echoes_bot/, "#USER#")
+#
+#   # send it back!
+#   reply src, tweet
+# end
 
 
 
